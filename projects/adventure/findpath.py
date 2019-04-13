d6d5fba10e898c1a6365b023dbace2746ff482c4
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-04-13 16:31:56 -0700

    NEW WORLD RECORD

--- projects/adventure/findpath.py
+++ projects/adventure/findpath.py
@@ -1,34 +1,35 @@
-import random
+from random import choices, choice, random
 
 
 class MapGraph:
-    def __init__(self, room):  # populate what is known, cxn == connections
+    def __init__(self, room):  # populate what is known in connections
         self.cxn, self.cxn[room.id] = {}, {}
-        for out in room.getExits():
+        for out in sorted(room.getExits(), key=lambda _: random()):
             self.cxn[room.id][out] = '?'
 
     def populate_known_areas(self, room, move, previous_room):
         if room.id not in self.cxn:
             self.cxn[room.id] = {}  # create new room in connections
-        for out in room.getExits():  # find and populate exits
+        for out in sorted(room.getExits(), key=lambda _: random()):
             if out not in self.cxn[room.id].keys():
-                self.cxn[room.id][out] = '?'
-        for out in previous_room.getExits():  # replace '?' with known numbers
-            if out == move:
+                self.cxn[room.id][out] = '?'  # find and populate unknown exits
+        for out in previous_room.getExits():
+            if out == move:  # replace '?' with known numbers if we moved from there
                 self.cxn[previous_room.id][out] = room.id
                 self.cxn[room.id][{'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}[
                     move]] = previous_room.id  # add reverse direction to previous room
 
     def path_to_next_q(self, room):  # Path like ['n', 'n'] to the closest ?
-        visited, queue = [], [[{room.id: None}]]
-        while queue:
-            path = queue.pop(random.choice([0, len(queue)-1]))  # bfs or dfs
+        visited, arr = [], [[{room.id: None}]]
+        while arr:  # randomly choose bfs or dfs by weight: [8, 2] respectively
+            # path = arr.pop(choices([0, len(arr)-1], [8, 2])[0])
+            path = arr.pop(0)
             address = list(path[-1].keys())[0]
             if address not in visited:
                 for news in self.cxn[address]:
                     new_path = list(path)
                     new_path.append({self.cxn[address][news]: news})
-                    queue.append(new_path)
+                    arr.append(new_path)
                     if self.cxn[address][news] == '?':
                         return [list(step.values())[0] for step in new_path[1:]]
                 visited.append(address)
